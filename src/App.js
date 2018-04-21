cc4cc09304a19e205548827afb49b2b1917e6b25
Wickramaranga Abeygunawardhana <umstek@live.com>
2018-04-21 09:43:30 +0530

    Upgrade project

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
-//noinspection JSUnresolvedVariable
-import 'antd/dist/antd.css';
-
-import React, {} from 'react';
-import Rx from 'rx';
-import Cycle from 'cycle-react';
+import React from 'react';
+import Rx from 'rxjs/Rx';
+import { component } from 'cycle-react/rxjs';
 
 import CountdownView from './CountdownView';
 
-//noinspection JSUnresolvedFunction
-let App = Cycle.component('App', function (interactions, props) {
-    return Rx.Observable.return(
-        <div className="App">
-            <CountdownView hours={0} minutes={25} seconds={0}/>
-        </div>
-    );
+let App = component('App', function(interactions, props) {
+  return Rx.Observable.of(
+    <div className="App">
+      <CountdownView hours={0} minutes={25} seconds={0} />
+    </div>
+  );
 });
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {LocaleProvider} from 'antd';
+import { LocaleProvider } from 'antd';
 import enUS from 'antd/lib/locale-provider/en_US';
+import 'antd/dist/antd.css';
 
 import App from './App';
 
 ReactDOM.render(
-    <LocaleProvider locale={enUS}>
-        <App/>
-    </LocaleProvider>,
-    document.getElementById('root')
+  <LocaleProvider locale={enUS}>
+    <App />
+  </LocaleProvider>,
+  document.getElementById('root')
 );
